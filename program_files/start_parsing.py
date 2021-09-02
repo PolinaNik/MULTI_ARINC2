432cd01eb595bac28e10c7ce46ccf2947140edfa
Polina Nikitich <polina-nikitich@yandex.ru>
2021-09-02 13:50:58 +1000

    отредактировала путь до файлов

--- program_files/start_parsing.py
+++ program_files/start_parsing.py
@@ -3,15 +3,15 @@ from sys import argv
 
 try:
     if argv[1] == "-h" or argv[1] == "?" or argv[1] == "--help":
-        os.system('python3 help.py')
+        os.system('python3 ./app/help.py')
     elif argv[1] == "-c" or argv[1] == "--check":
-        os.system('python3 checking.py')
+        os.system('python3 ./app/checking.py')
     elif argv[1] == "-s" or argv[1] == "--sintez":
-        os.system('python3 sintez_parsing.py')
+        os.system('python3 ./app/sintez_parsing.py')
     elif argv[1] == "-k" or argv[1] == "--korinf":
-        os.system('python3 korinf_parsing.py')
+        os.system('python3 ./app/korinf_parsing.py')
     elif argv[1] == "o" or argv[1] == "--orla":
-        os.system('python3 radar_screen_orl_parsing.py')
+        os.system('python3 ./app/radar_screen_orl_parsing.py')
     else:
         print(f'Вы ввели неправильный ключ, для справочной информации\n'
               f'введите -h, --help или ?')
